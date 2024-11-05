57ebf8dbc5fe76a3fdc4783fabaa8aa3194c0565
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-11-05 08:55:32 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,4 +2,6 @@ const notif = (title, body, icon) => { // Icons don't properly work, they just t
   window.Notification?"granted"===Notification.permission?serviceWorkerRegistration.showNotification(title,{body:body,icon:icon}):Notification.requestPermission().then(function(o){"granted"===o?serviceWorkerRegistration.showNotification(title,{body:body,icon:icon}):console.log("User blocked notifications.")}).catch(function(o){console.error(o)}):console.log("Browser does not support notifications.");
 }
 
-notif('Title', 'Body', '');
+document.querySelector('#notif').addEventListener('click', () => {
+  notif('Title', 'Body', '');
+});
