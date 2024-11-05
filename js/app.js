f25d28902e9dc21d97e95082128dd0e380e2c35e
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-11-05 12:35:14 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,24 @@ const notif = (title, body, icon) => { // Icons don't properly work, they just t
   window.Notification?"granted"===Notification.permission?serviceWorkerRegistration.showNotification(title,{body:body,icon:icon}):Notification.requestPermission().then(function(o){"granted"===o?serviceWorkerRegistration.showNotification(title,{body:body,icon:icon}):console.log("User blocked notifications.")}).catch(function(o){console.error(o)}):console.log("Browser does not support notifications.");
 }
 
-document.querySelector('#notif').addEventListener('click', () => {
+document.querySelector('#notif').onclick = () => {
   notif('Title', 'Body', '');
 });
+
+document.querySelector('#dis').innerText = localStorage.getItem('data') ? localStorage.getItem('data') : 'None';
+
+document.querySelector('b1').onclick = () => {
+  localStorage.setItem('data', 1);
+}
+
+document.querySelector('b2').onclick = () => {
+  localStorage.setItem('data', 2);
+}
+
+document.querySelector('b3').onclick = () => {
+  localStorage.setItem('data', 3);
+}
+
+document.querySelector('cls').onclick = () => {
+  localStorage.setItem('data', undefined);
+}
