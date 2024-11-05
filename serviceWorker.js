1e4d598779f1bb886efacecab6c53d23c0ffb333
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-11-05 08:08:04 -0600

    Create serviceWorker.js

---
+++ serviceWorker.js
@@ -0,0 +1,52 @@
+const cacheName = 'client'
+const assets = [
+  "/",
+  "/index.html",
+  "/images/icon512_maskable.png",
+  "/images/icon512_rounded.png",
+  "/css/style.css",
+  "/js/app.js"
+]
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close(); // Do anything here
+  clients.openWindow("https://www.google.com"); // On app I think
+});
+
+// Installing the Service Worker
+self.addEventListener("install", async (event) => {
+  try {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(assets);
+  } catch (error) {
+    console.error("Service Worker installation failed:", error);
+  }
+});
+
+// Fetching resources
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(cacheName);
+
+      try {
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse) {
+          console.log("cachedResponse: ", event.request.url);
+          return cachedResponse;
+        }
+
+        const fetchResponse = await fetch(event.request);
+        if (fetchResponse) {
+          console.log("fetchResponse: ", event.request.url);
+          await cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        }
+      } catch (error) {
+        console.log("Fetch failed: ", error);
+        const cachedResponse = await cache.match("index.html");
+        return cachedResponse;
+      }
+    })()
+  );
+});
